e08b4447e5b9775bfab5a44618b99c85472ebaba
Tuan Nguyen <Nguyenvantuan.itus@gmail.com>
2016-10-23 15:05:33 +0700

    Convert to MEAN structure

---
+++ public/application.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var app = angular.module('lotusAirline', [
+    'ngRoute',
+    'ngSanitize',
+    'lotusAirline.search',
+    'lotusAirline.ticketResults',
+    'lotusAirline.passengerInfor',
+    'lotusAirline.submition']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: 'views/search-flight.html',
+        controller: 'SearchCtrl'
+
+    }).when('/ticket-results', {
+        templateUrl: 'views/ticket-result.html',
+        controller: 'TicketResultCtrl'
+
+    }).when('/get-passengers-information', {
+        templateUrl: 'views/passenger-information.html',
+        controller: 'PassengersCtrl'
+
+    }).when('/search', {
+        templateUrl: 'views/submition.html',
+        controller: 'SubmitionCtrl'
+
+    }).otherwise({
+        redirectTo: '/'
+    });
+}]);
+
