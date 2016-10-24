bb2842bbae730c45e266cf5b481e113793cdab6c
Tuan Nguyen <Nguyenvantuan.itus@gmail.com>
2016-10-24 20:52:34 +0700

    Add booking information

--- public/controllers/ticket-result.js
+++ public/controllers/ticket-result.js
@@ -3,22 +3,57 @@
  */
 
 var ticketResultModule = angular.module('lotusAirline.ticketResults', []);
-
-ticketResultModule.controller('TicketCtrl', ['$scope', '$window', '$http', '$rootScope', '$location',
-    function ($scope, $window, $http, $rootScope, $location) {
+ticketResultModule.controller('TicketCtrl', ['$scope', '$window', '$http', '$rootScope', '$location', '$timeout',
+    function ($scope, $window, $http, $rootScope, $location, $timeout) {
 
         $scope.currIndex = -1;
 
-        $scope.showTicketDetail = function ($index) {
-
-            getTotalCost();
-
-            // Show ticket detail
-            $scope.currIndex = $index;
+        // Booking infor
+        $rootScope.bookingInfor = {
+            "flights": [
+                {
+                    "code": "",
+                    "departAt": 0,
+                    "arriveAt": 0,
+                    "priceLevel": "",
+                    "class": ""
+                }
+            ],
+            "passengers": [
+                {
+                    "title": "",
+                    "firstName": "",
+                    "lastName": ""
+                }
+            ]
         };
 
-        function getTotalCost() {
-            
+        $scope.selectTicket = function ($index, type) {
+            if (type == 'depart')
+                $scope.departTicket = $rootScope.departTickets[$index];
+            else
+                $scope.returnTicket = $rootScope.returnTickets[$index];
+
+            updateBookingInfor();
+            console.log($scope.bookingInfor);
+            console.log($scope.departTicket);
+            console.log($scope.returnTicket);
+        };
+
+        function updateBookingInfor() {
+            // Update depart root
+            $scope.bookingInfor.flights[0].code = $scope.departTicket.code;
+            $scope.bookingInfor.flights[0].departAt = $scope.departTicket.departAt;
+            $scope.bookingInfor.flights[0].arriveAt = $scope.departTicket.arriveAt;
+            $scope.bookingInfor.flights[0].priceLevel = $scope.departTicket.priceLevel;
+            $scope.bookingInfor.flights[0].class = $scope.departTicket.class;
+
+            // Update arrival root
+            // $scope.bookingInfor.flights[1].code = $scope.returnTicket.code;
+            // $scope.bookingInfor.flights[1].departAt = $scope.returnTicket.departAt;
+            // $scope.bookingInfor.flights[1].arriveAt = $scope.returnTicket.arriveAt;
+            // $scope.bookingInfor.flights[1].priceLevel = $scope.returnTicket.priceLevel;
+            // $scope.bookingInfor.flights[1].class = $scope.returnTicket.class;
         }
 
         $scope.getTime = function (timestamp) {
@@ -26,8 +61,8 @@ ticketResultModule.controller('TicketCtrl', ['$scope', '$window', '$http', '$roo
             var hour = date.getHours();
             var minute = date.getMinutes();
 
-            if(hour < 10) hour = '0' + hour;
-            if(minute < 10) minute = '0' + minute;
+            if (hour < 10) hour = '0' + hour;
+            if (minute < 10) minute = '0' + minute;
 
             return hour + ":" + minute;
         };
@@ -38,8 +73,8 @@ ticketResultModule.controller('TicketCtrl', ['$scope', '$window', '$http', '$roo
             var mm = date.getMonth();
             var yyyy = date.getYear();
 
-            if(dd < 10) dd = '0' + dd;
-            if(mm < 10) mm = '0' + mm;
+            if (dd < 10) dd = '0' + dd;
+            if (mm < 10) mm = '0' + mm;
 
             return dd + "/" + mm + "/" + yyyy;
         };
